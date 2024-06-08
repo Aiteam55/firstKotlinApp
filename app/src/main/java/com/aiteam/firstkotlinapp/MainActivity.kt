53cb1e0ca2e04dcf67ed7a3ed0da9f6c50bd5814
Aiteam55 <mitiaandria1@gmail.com>
2024-06-08 11:29:57 +0300

    Simple add name app

--- app/src/main/java/com/aiteam/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/aiteam/firstkotlinapp/MainActivity.kt
@@ -4,27 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Add
-import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.aiteam.firstkotlinapp.ui.theme.FirstKotlinAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,11 +34,41 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FirstKotlinAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Mitia",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                var name by remember {
+                    mutableStateOf("")
+                }
+                var listName by remember {
+                    mutableStateOf(listOf<String>())
+                }
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 16.dp, vertical = 32.dp)
+                ) {
+                    Title()
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                    ) {
+                        OutlinedTextField(
+                            value = name,
+                            onValueChange = { text ->
+                                name = text
+                            },
+                            modifier = Modifier.weight(1f)
+                        )
+                        Spacer(modifier = Modifier.width(16.dp))
+                        Button(
+                            onClick = {
+                                listName += name
+                                name = ""
+                            }
+                        )
+                        {
+                            Text(text = "Add")
+                        }
+                    }
+                    NameList(listName = listName)
                 }
             }
         }
@@ -45,43 +76,28 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    LazyColumn {
-        items(10) {
-            i -> Icon(imageVector = Icons.Default.Add, contentDescription = null)
-        }
-    }
-//    Column {
-//        for(i in 1..10) {
-//            Icon(imageVector = Icons.Default.Add, contentDescription = null)
-//        }
-//    }
-//    Image(
-//        painter = painterResource(id = R.drawable.ic_launcher_foreground),
-//        contentDescription = null,
-//        modifier = Modifier.background(Color.Black)
-//    )
-//    Column (
-//        horizontalAlignment = Alignment.CenterHorizontally,
-//        verticalArrangement = Arrangement.Center,
-//        modifier = Modifier
-//            .size(400.dp)
-//    ){
-//        Text(
-//            text = "Hello $name!",
-//            modifier = modifier
-//        )
-//        Text(
-//            text = "Some other text lavalava be",
-//            modifier = modifier
-//        )
-//    }
+fun Title(modifier: Modifier = Modifier) {
+    Text(
+        text = "Add name",
+        fontSize = 24.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(vertical = 16.dp)
+    )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    FirstKotlinAppTheme {
-        Greeting("Android")
+fun NameList(listName: List<String>, modifier: Modifier = Modifier) {
+    LazyColumn {
+        items(listName) { currentName ->
+            Text(
+                text = currentName,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+            Divider()
+        }
     }
 }
